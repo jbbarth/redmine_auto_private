34ca44496e35db8b4054fe34014f9fad9384a240
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-21 15:26:12 +0200

    Switch the test suite to rspec

--- spec/controllers/issues_controller_auto_private_spec.rb
+++ spec/controllers/issues_controller_auto_private_spec.rb
@@ -1,6 +1,7 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require 'spec_helper'
 
-class IssuesControllerAutoPrivateTest < ActionController::TestCase
+describe IssuesController do
+  render_views
   fixtures :projects, :users, :roles, :members, :member_roles, :issues, :issue_statuses, :versions,
            :trackers, :projects_trackers, :issue_categories, :enabled_modules, :enumerations, :attachments,
            :workflows, :custom_fields, :custom_values, :custom_fields_projects, :custom_fields_trackers,
@@ -8,7 +9,7 @@ class IssuesControllerAutoPrivateTest < ActionController::TestCase
 
   include Redmine::I18n
 
-  setup do
+  before do
     @controller = IssuesController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
@@ -16,17 +17,17 @@ class IssuesControllerAutoPrivateTest < ActionController::TestCase
     @request.session[:user_id] = 1 #permissions are hard
   end
 
-  test "keeps checkbox if auto-private flag not set" do
+  it "should keeps checkbox if auto-private flag not set" do
     Issue.find(1).project.update_attribute(:force_private_issues, false)
     get :show, :id => 1
-    assert_response :success
+    response.should be_success
     assert_tag "input", :attributes => {:name => "issue[is_private]", :type => "checkbox"}
   end
 
-  test "set private flag for issues" do
+  it "should set private flag for issues" do
     Issue.find(1).project.update_attribute(:force_private_issues, true)
     get :show, :id => 1
-    assert_response :success
+    response.should be_success
     assert_no_tag "input", :attributes => {:name => "issue[is_private]", :type => "checkbox"}
     assert_tag "input", :attributes => {:name => "issue[is_private]", :type => "hidden", :value => "1"}
   end
--- spec/models/project_patch_spec.rb
+++ spec/models/project_patch_spec.rb
@@ -1,10 +1,10 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require 'spec_helper'
 require File.dirname(__FILE__) + '/../../lib/redmine_auto_private/project_patch'
 
-class ProjectPatchTest < ActiveSupport::TestCase
+describe "ProjectPatch" do
   fixtures :projects
   
-  test "Project#force_private_issues" do
+  it "should Project#force_private_issues" do
     project = Project.find(1)
     project.safe_attributes=({ "force_private_issues" => 1 })
     assert project.force_private_issues
