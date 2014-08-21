86cb6cff9342f25912b3429a17decae99c5521a0
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-21 19:02:12 +0200

    Add comments to help my future-self

--- init.rb
+++ init.rb
@@ -22,5 +22,7 @@ Redmine::Plugin.register :redmine_auto_private do
   requires_redmine :version_or_higher => '2.1.0'
   version '0.0.1'
   url 'https://github.com/jbbarth/redmine_auto_private'
-  #requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
+  #doesn't work since redmine evaluates dependencies as it loads, and loads in lexical order
+  #TODO: see if it works in Redmine 2.6.x or 3.x when they're released
+  # requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
 end
