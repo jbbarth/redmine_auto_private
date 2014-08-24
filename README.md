9522d31904622281c077efd70066d9d1ecc4356f
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-24 05:53:03 +0200

    Depend on redmine_base_deface and remove dirty hacks for deface integration

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ may not understand the implications of checking "Private" on their issues, or th
 just forget it, which leads to unecessary information leaks accross the project. This plugin
 acts as a safety net for those issues by automatically setting every issue "Private" flag
 to "true" on Issue creation and update.
+
+Install
+-------
+
+You can first take a look at general instructions for plugins [here](http://www.redmine.org/wiki/redmine/Plugins).
+
+Then :
+* install the redmine_base_deface plugin (see [here](https://github.com/jbbarth/redmine_base_deface))
+* clone this repository in your "plugins/" directory ; if you have a doubt you put it at the good level, you can check you have a plugins/redmine_auto_private/init.rb file
+* install dependencies (gems) by running `bundle install` from the root of your redmine instance
+* migrate the database by running `rake redmine:plugins` from the root of your redmine instance
+* restart your Redmine instance (depends on how you host it)
--- init.rb
+++ init.rb
@@ -6,14 +6,6 @@ ActionDispatch::Callbacks.to_prepare do
   require_dependency 'redmine_auto_private/project_patch'
 end
 
-# Little hack for using the 'deface' gem in redmine:
-# - redmine plugins are not railties nor engines, so deface overrides in app/overrides/ are not detected automatically
-# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
-# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
-# TODO: see if it's complicated to turn a plugin into a Railtie or find something a bit cleaner
-Rails.application.paths["app/overrides"] ||= []
-Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
-
 Redmine::Plugin.register :redmine_auto_private do
   name 'Redmine Auto Private plugin'
   description 'This plugin forces private issues for the projects you want'
@@ -25,4 +17,5 @@ Redmine::Plugin.register :redmine_auto_private do
   #doesn't work since redmine evaluates dependencies as it loads, and loads in lexical order
   #TODO: see if it works in Redmine 2.6.x or 3.x when they're released
   # requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
+  # requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
 end
