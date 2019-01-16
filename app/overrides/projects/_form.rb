5d1c09b54b7e87788ae7112bc5712319224c1d99
Vincent Robert <vincent.robert@nanego.com>
2019-01-16 16:21:43 +0100

    Fix deface override broken in Redmine 4

--- app/overrides/projects/_form.rb
+++ app/overrides/projects/_form.rb
@@ -1,13 +1,14 @@
-Deface::Override.new :virtual_path  => 'projects/_form',
+Deface::Override.new :virtual_path  => 'projects/settings/_issues',
                      :name          => 'add-auto-private-issues-option-to-project',
-                     :insert_bottom => '#project_trackers',
+                     :insert_before => "erb[loud]:contains('submit_tag')",
                      :text          => %(
-                       <div class="clear"></div>
-                       <div style="margin-top:15px; padding:8px 0; border-top:1px solid #ddd;">
-                         <label style="margin-left:0; font-weight:normal; width:auto;">
-                           <%= hidden_field_tag 'project[force_private_issues]', 0 %>
-                           <%= check_box_tag 'project[force_private_issues]', 1, @project.force_private_issues %>
-                           <%= l(:label_force_private_issues) %>
-                         </label>
+                       <div class="box tabular">
+                         <p>
+                           <label style="margin-left:0; font-weight:normal; width:auto;">
+                             <%= hidden_field_tag 'project[force_private_issues]', 0 %>
+                             <%= check_box_tag 'project[force_private_issues]', 1, @project.force_private_issues %>
+                             <%= l(:label_force_private_issues) %>
+                           </label>
+                         </p>
                        </div>
                      )
