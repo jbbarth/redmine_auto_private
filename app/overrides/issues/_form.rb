acd3da2c072a9315e3d8cdbae05dffd3cd7c8874
Vincent Robert <vincent.robert@nanego.com>
2019-11-04 11:37:59 +0100

    Improve compatibility with redmine_customize_core_fields plugin

--- app/overrides/issues/_form.rb
+++ app/overrides/issues/_form.rb
@@ -1,4 +1,11 @@
 Deface::Override.new :virtual_path  => 'issues/_form',
                      :name          => 'auto-set-private-flag',
+                     :original      => '8f374ec2439b27545906aae44228462dfe14d196',
+                     :replace       => 'p:contains("is_private")',
+                     :partial       => 'redmine_auto_private/form'
+
+Deface::Override.new :virtual_path  => 'issues/_form_with_positions',
+                     :name          => 'auto-set-private-flag',
+                     :original      => '8f374ec2439b27545906aae44228462dfe14d196',
                      :replace       => 'p:contains("is_private")',
                      :partial       => 'redmine_auto_private/form'
