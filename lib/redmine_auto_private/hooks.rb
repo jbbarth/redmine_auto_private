69b2d88e66f335b22e98f402b5a9f24a30c1ff65
mathieu-mbru <m.brulin@adacis.net>
2022-01-25 10:53:13 +0100

    Forcer l'attribut is_private à true lors de la création de la demande, même si l'utilisateur n'a pas cette permission.

--- lib/redmine_auto_private/hooks.rb
+++ lib/redmine_auto_private/hooks.rb
@@ -1,5 +1,17 @@
 module RedmineAutoPrivate
   class Hooks < Redmine::Hook::ViewListener
-    #TODO
+
+    def controller_issues_new_before_save(context={})
+      issue = context[:issue]
+
+      if ((issue.author_id == User.current.id) &&
+         (!((User.current.allowed_to?(:set_own_issues_private, issue.project)) || User.current.allowed_to?(:set_issues_private, issue.project))) &&
+         (issue.project.force_private_issues))
+
+        issue.is_private = true
+      end
+
+    end
+
   end
 end
