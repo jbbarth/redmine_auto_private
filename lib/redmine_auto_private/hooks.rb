ef015d949d178f9cffcb1e7d8fc24109ad72b3da
Vincent Robert <vincent.robert@nanego.com>
2022-05-11 09:48:27 +0200

    Avoid bug when issue project is not known yet

--- lib/redmine_auto_private/hooks.rb
+++ lib/redmine_auto_private/hooks.rb
@@ -2,7 +2,7 @@ module RedmineAutoPrivate
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_new_before_save(context = {})
       issue = context[:issue]
-      issue.is_private = true if issue.project.force_private_issues
+      issue.is_private = true if issue&.project&.force_private_issues
     end
   end
 
