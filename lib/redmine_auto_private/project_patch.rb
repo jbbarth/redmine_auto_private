56c69d14339edb9da69ced71d3a23d5b64c3d9a1
Yazan <y.alaeddin@adacis.net>
2022-06-02 14:40:58 +0200

    Copier l'option force_private_issues lorsque une copie d'un project + test

--- lib/redmine_auto_private/project_patch.rb
+++ lib/redmine_auto_private/project_patch.rb
@@ -5,3 +5,15 @@ class Project
 
   safe_attributes 'force_private_issues'
 end
+module PluginAutoPrivate
+  module ProjectPatch
+    def copy(project, options={})
+      super
+      project = project.is_a?(Project) ? project : Project.find(project)
+      self.force_private_issues = project.force_private_issues
+      save
+    end
+  end
+end
+
+Project.prepend PluginAutoPrivate::ProjectPatch
