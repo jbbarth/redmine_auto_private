2b2c04bcba89a00e1f43afd05369636812aa7966
Vincent Robert <vincent.robert@nanego.com>
2024-02-28 08:52:56 +0100

    Update CI config and add latest Redmine version

--- init.rb
+++ init.rb
@@ -5,7 +5,6 @@ Redmine::Plugin.register :redmine_auto_private do
   name 'Redmine Auto Private plugin'
   description 'This plugin forces private issues for the projects you want'
   author 'Jean-Baptiste BARTH'
-  author_url 'mailto:jeanbaptiste.barth@gmail.com'
   requires_redmine :version_or_higher => '2.1.0'
   version '0.0.1'
   url 'https://github.com/jbbarth/redmine_auto_private'
